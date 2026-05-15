eae26836218f2a77b2e145b922f7a9ae2c52262a
Moeed <moeedulhaq@gmail.com>
2026-05-15 12:20:08 +0500

    Committed

--- src/pages/moeed/AboutUs.jsx
+++ src/pages/moeed/AboutUs.jsx
@@ -6,20 +6,27 @@ import masterStyle from './master.module.css';
 import aboutUsStyle from './aboutUs.module.css'
 
 
-function MemberIntroduction({ role, name, p, src, alt }) {
+function MemberIntroduction({ role, name, p, src, alt, courses}) {
     return (
         <div className={`${aboutUsStyle.memberCard} ${masterStyle.flexRow}`}>
             <div className={`${aboutUsStyle.memberCardLeft} ${masterStyle.flexColumn}`}>
                 <img src={src} alt={alt} />
                 <h3 className={`${masterStyle.lineBeforeText}`}>{name}</h3>
             </div>
-            <div className={` ${aboutUsStyle.memberCardRight} ${masterStyle.flexColumn}`}>
-                <h2 className={`${masterStyle.headingText}`}>{role}</h2>
+            <div className={`${aboutUsStyle.memberCardRight} ${masterStyle.flexColumn}`}>
+                <div className={`${aboutUsStyle.memberCardRightTop} ${masterStyle.flexRow}`}>
+                    <h2 className={`${masterStyle.headingText}`}>{role}</h2>
+                    <div className={masterStyle.flexRow}>
+                        <SecondaryLinkButton href={'#'} text={'CEH'} />
+                        <SecondaryLinkButton href={'#'} text={'eWPT'} />
+                        <SecondaryLinkButton href={'#'} text={'CISSP'} />
+                    </div>
+                </div>
                 <p className={`${masterStyle.normalText}`}>{p}</p>
                 <div className={`${aboutUsStyle.socialLinks} ${masterStyle.flexRow}`}>
-                    <a href=""><img src="src/assets/pictures/1.png" alt="F" /></a>
-                    <a href=""><img src="src/assets/pictures/1.png" alt="F" /></a>
-                    <a href=""><img src="src/assets/pictures/1.png" alt="F" /></a>
+                    <a href=""><img src="src/assets/Social Icons/linkedin-logo.png" alt="F" /></a>
+                    <a href=""><img src="src/assets/Social Icons/github-logo.png" alt="F" /></a>
+                    <a href=""><img src="src/assets/Social Icons/twitter-logo.png" alt="F" /></a>
                 </div>
             </div>
         </div>
@@ -32,10 +39,9 @@ function AboutUs() {
 
             <div className={`${aboutUsStyle.memberCardsHolder}`}>
                 <MemberIntroduction role={'Web Pentester'} name={'Moeed Ul Haq (BS)'} p={<>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim voluptates voluptatibus laboriosam ut deleniti! Similique provident aspernatur facere, excepturi perspiciatis dicta nostrum qui quia iusto ipsa mollitia eum magnam repellendus!</>} src={'src/assets/pictures/Moeed_Pic.jpg'} alt={'Moeed Pic'} />
-
                 <MemberIntroduction role={'Web Pentester'} name={'Hasnat Farooq (BS)'} p={<>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim voluptates voluptatibus laboriosam ut deleniti! Similique provident aspernatur facere, excepturi perspiciatis dicta nostrum qui quia iusto ipsa mollitia eum magnam repellendus!</>} src={'src/assets/pictures/Hasnat_Pic.png'} alt={'Hasnat Pic'} />
-
                 <MemberIntroduction role={'Web Pentester'} name={'Ahmad Abdullah (BS)'} p={<>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim voluptates voluptatibus laboriosam ut deleniti! Similique provident aspernatur facere, excepturi perspiciatis dicta nostrum qui quia iusto ipsa mollitia eum magnam repellendus!</>} src={'src/assets/pictures/Ahmad_Pic.jpeg'} alt={'Ahmad Pic'} />
+                <MemberIntroduction role={'Web Pentester'} name={'Moeed Ul Haq (BS)'} p={<>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim voluptates voluptatibus laboriosam ut deleniti! Similique provident aspernatur facere, excepturi perspiciatis dicta nostrum qui quia iusto ipsa mollitia eum magnam repellendus!</>} src={'src/assets/pictures/Moeed_Pic.jpg'} alt={'Moeed Pic'} />
             </div>
         </section>
     )
